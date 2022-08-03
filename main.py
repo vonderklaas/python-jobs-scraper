7068f9dd1785cec539a1e6bc8d489218edf72d36
Nick Garbalau <garbalaunick@protonmail.com>
2022-08-03 16:28:08 +0300

    Scraping in action

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 with open('home.html') as html_file:
     content = html_file.read()
     soup = BeautifulSoup(content, 'lxml')
-    
-    tags = soup.findAll('h5')
-    for tag in tags:
-        print(tag)
+    course_cards = soup.find_all('div', class_='card')
+
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(f'{course_name} costs {course_price}')
